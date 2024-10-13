31ea9c264723056f5d886d7dbe54a66c00e2e31a
Leandro Tavares <149838874+Leandrobftt@users.noreply.github.com>
2024-10-12 22:52:44 -0300

    Add files via upload

--- src/db/DDL.sql
+++ src/db/DDL.sql
@@ -1,1 +1,29 @@
+-- Criação do banco de dados 
+CREATE DATABASE AdotaPet;
+USE AdotaPet;
 
+-- Criando as tabelas do banco de dados
+CREATE TABLE Usuario (
+Cod_usuario char(3) PRIMARY KEY,
+ Nome char(40)
+ );
+
+CREATE TABLE Anuncio (
+	Cod_usuario char(3) PRIMARY KEY,
+    Nome char(40)
+    );
+
+CREATE TABLE Pet (
+	Cod_Pet char(3) PRIMARY KEY,
+    Nome_Pet char(40),
+    Anuncio char(3),
+    Usuario char(3),
+    Contato char(13),
+    CONSTRAINT FK_Pet_Anuncio foreign key (Anuncio) REFERENCES Anuncio(Cod_usuario)
+    );
+
+CREATE TABLE Contato (
+Cod_usuario char(13) PRIMARY KEY,
+	Telefone varchar(11),
+    Email varchar(40)
+    );
